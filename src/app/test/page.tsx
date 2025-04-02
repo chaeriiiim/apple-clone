923c53e8bd5e5edb2a87d95dc719a12ce49572ad
김채림 <woqja0905@naver.com>
2025-04-02 20:06:36 +0900

    update: FeatureHero css 수정

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -7,19 +7,21 @@ export default function page() {
   return (
     <main className="w-screen">
       <h1 className="text-xl font-bold mb-4">🧪 테스트 페이지</h1>
-      <FeatureHero
-        imageSrc="/FeatureImage.svg"
-        alt="Feature Image"
-        className=" h-[690px]"
-      >
-        <Button
-          buttonType="button"
-          onClick={() => console.log("clicked")}
-          className="pl-[15px] pr-[15px] pt-[11px] pb-[11px]  w-[114px] h-[44px] "
+      <div className="mt-[13px]">
+        <FeatureHero
+          imageSrc="/FeatureImage.svg"
+          alt="Feature Image"
+          className=" h-[690px] "
         >
-          더 알아보기
-        </Button>
-      </FeatureHero>
+          <Button
+            buttonType="button"
+            onClick={() => console.log("clicked")}
+            className="pl-[15px] pr-[15px] pt-[11px] pb-[11px]  w-[114px] h-[44px] "
+          >
+            더 알아보기
+          </Button>
+        </FeatureHero>
+      </div>
     </main>
   );
 }
